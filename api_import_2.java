705055ff3b659053733ab0359d8a4bfd8abc5e4a
Alexander Filippov <alexf258369@gmail.com>
2020-03-14 12:33:11 +0200

    The benefit of Java API example

---
+++ api_import_2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class api_import_2 {
+    
+    public static void main(String args[]) {
+        ArrayList<String> a = new ArrayList<String>();
+        TreeSet<String> t = new TreeSet<String>();
+    }
+}
