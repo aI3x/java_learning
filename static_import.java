130e25aeafdc70b1093123cc25503e301223bd1b
Alexander Filippov <alexf258369@gmail.com>
2020-03-14 12:33:40 +0200

    Life of static import

---
+++ static_import.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+import static java.lang.Integer.*;
+
+public class static_import {
+
+    public static void main(String args[]) {
+        out.println(MAX_VALUE);
+        out.println(toHexString(42));
+    }
+}
